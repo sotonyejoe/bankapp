153556aa890a6b0a4820f1328b975d1a11425449
SotonyeJoe <sotonye28@gmail.com>
2026-05-28 01:48:37 +0100

    first commit

--- wave/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import "./App.css"
-
-export default function App() {
-  return (
-    <div className='text-5xl text-center m-10 p-10 font-bold'>Hello Wold!!!</div>
-  )
-}
